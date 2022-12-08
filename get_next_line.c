7b2f8aa7bf9003d1c838e61c6a82148cd5b94c57
jonathan <jo@DESKB.localdomain>
2022-12-08 04:57:20 +0100

    work

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joterret <joterret@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jo <jo@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 04:01:24 by jo                #+#    #+#             */
-/*   Updated: 2022/12/07 11:39:19 by joterret         ###   ########.fr       */
+/*   Updated: 2022/12/08 03:55:20 by jo               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 char	*get_next_line(int fd)
 {		
-	static char str_saved[BUFFER_SIZE + 1] = "";
-	char		str_tmp[BUFFER_SIZE + 1];
-	char		*str_file = NULL;
+	static t_list	*stash = NULL;
+	char			*line;
 	ssize_t		nb_readed_bytes;
+
+	if (fd < 0 || BUFFER_SIZE < 0 || read(fd, &line, 0) < 0)
+		return (0);
+
 
 	//NOTE -  - 1: Parcourir le fichier (fd)
 	//TODO - 2: Copie le fichier dans la string temporaire
 	//TODO - 3: Parcour la string temporaire jusqua trouver un /n et la retourne en temp que ligne
+
+
+
 
 
 
@@ -32,7 +38,7 @@ char	*get_next_line(int fd)
 		nb_readed_bytes = read(fd, str_tmp, BUFFER_SIZE);
 		str_tmp[nb_readed_bytes] = '\0';
 
-		if (str_file == NULL)//FIXME - corrriger cette merde
+		if (str_file == NULL)
 			str_file = ft_strjoin("", str_tmp);
 		else
 			str_file = ft_strjoin()
--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joterret <joterret@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jo <jo@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 03:08:18 by joterret          #+#    #+#             */
-/*   Updated: 2022/12/07 11:19:12 by joterret         ###   ########.fr       */
+/*   Updated: 2022/12/08 04:48:39 by jo               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GNL_H
 # define FT_GNL_H
 
-//SECTION - Include - ---------------------------------------------------------
+//NOTE - Include 
 #include	<unistd.h>
 #include	<stdio.h>
 #include	<fcntl.h>
@@ -22,10 +22,10 @@
 #include	<unistd.h>
 #include	<stdlib.h> 
 
-//SECTION - Fonction principal-------------------------------------------------
+//NOTE - Fonction principal
 char		*get_next_line(int fd);
 
-//SECTION -  - Fonction suplementaire------------------------------------------------
+//NOTE - Fonction suplementaire
 size_t	gnl_retlen(char *str);
 size_t	gnl_strlen(const char *str);
 int		gnl_find_newline(char *str);
@@ -34,9 +34,20 @@ int		gnl_find_zero(char *str);
 size_t	gnl__strlcpy(char *dest, const char *src, size_t dstsize);
 char	*gnl_strjoin(char const *s1, char const *s2);
 
-#endif
 
-//NOTE - Buffer size-----------------------------------------------------------
+
+//NOTE - Buffer size
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 100
+#endif
+
+//NOTE - Struct
+typedef s_list
+{
+    char            *contenu;
+    struct s_list   *next;
+
+}                   t_list;
+
+
 #endif
